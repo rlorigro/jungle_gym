15cbe85f5f1afc940d917cf4d488e883d11df296
rlorigro <28764332+rlorigro@users.noreply.github.com>
2024-04-19 02:21:37 -0700

    Distributed test

--- scratch.py
+++ scratch.py
@@ -1,29 +1,38 @@
 import os
 import torch
 import torch.distributed.rpc as rpc
+import torch.multiprocessing as mp
 
 print(torch.__version__)
 print(torch.distributed.is_available())
 print(rpc.is_available())
 
 
-def run():
+def run(rank, world_size):
     print("torch.distributed.is_initialized()", torch.distributed.is_initialized())
 
-    init_method = "file:///tmp/rpc"
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '29500'
 
-    if os.path.exists(init_method):
-        os.remove(init_method)
+    rpc.init_rpc(
+        "worker_"+str(rank),
+        world_size=world_size,
+        rank=rank)
 
-    torch.distributed.init_process_group(
-        "gloo",
-        init_method=init_method,
-        rank=0,
-        world_size=4)
+    print("torch.distributed.is_initialized()", torch.distributed.is_initialized())
+
+    rpc.shutdown()
 
 
 def main():
-    run()
+    a = mp.Process(target=run, args=(0,2))
+    b = mp.Process(target=run, args=(1,2))
+
+    a.start()
+    b.start()
+
+    a.join()
+    b.join()
 
 
 if __name__ == "__main__":
