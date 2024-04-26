841be0ee7e435aa79d6de226490915c693d5505a
rlorigro <28764332+rlorigro@users.noreply.github.com>
2024-04-26 03:05:55 -0700

    Distributed test working with gradients!

--- scratch.py
+++ scratch.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 
 import torch
 import random
@@ -38,13 +39,55 @@ def conditional_rerun(batch, batch_size, rank):
     return
 
 
+def generate_training_set(n, iw=-1):
+    x = list()
+    y = list()
+
+    for i in range(n):
+        x.append(random.random() * 9)
+        y.append(math.sin(x[-1]))
+
+    # print(x)
+    # print(y)
+
+    x = torch.Tensor(x)
+    y = torch.Tensor(y)
+
+    # print(x.shape)
+    # print(y.shape)
+    # print(x.shape)
+    # print(y.shape)
+
+    return x, y
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+        self.linear1 = torch.nn.Linear(1, 128)
+        self.activation = torch.nn.ReLU()
+        self.linear2 = torch.nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.linear2(x)
+        return x
+
+
 def consumer_function(rank, world_size):
     if rank != 0:
         exit("ERROR: Consumer rank must be 0")
 
     initialize(rank, world_size)
+    # initialize here
 
-    batch_size = 16
+    model = Model()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-4)
+    loss_fn = torch.nn.MSELoss()
+
+    batch_size = 8
     batch = list()
 
     print(world_size)
@@ -52,7 +95,7 @@ def consumer_function(rank, world_size):
     futures = dict()
 
     for i in range(1, world_size):
-        futures[i] = rpc.rpc_async(i, producer_function, args=(i,))
+        futures[i] = rpc.rpc_async(i, producer_function, args=(i, model))
 
     done = False
     while not done:
@@ -64,29 +107,44 @@ def consumer_function(rank, world_size):
                 batch.append(future.value())
 
                 if len(batch) < batch_size:
-                    futures[i] = rpc.rpc_async(i, producer_function, args=(i,))
+                    # pass new model here
+                    futures[i] = rpc.rpc_async(i, producer_function, args=(i, model))
                 else:
 
                     done = True
                     break
 
-    print(batch)
+    y_batch = torch.stack(list(zip(*batch))[0], dim=0)
+    y_predict_batch = torch.stack(list(zip(*batch))[1], dim=0)
+
+    print(y_batch)
+    print(y_batch[0].shape)
+    print(y_batch.shape)
+    print(y_predict_batch.shape)
+
+    loss = loss_fn(y_predict_batch, y_batch)
+
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
 
     rpc.shutdown()
 
 
-def producer_function(i):
-    print("launching", i)
-    while True:
-        n = int(random.random() * 100)
+def producer_function(rank, model):
+    # catch latest model
+    # run training
+    # spit out win
+    #
+    x, y = generate_training_set(1, rank)
 
-        if n > 50:
-            print("yippee ", n, " ", i)
-            return (n, i)
+    y_predict = model.forward(x)
+
+    return y, y_predict
 
 
 def main():
-    n_processes = 4
+    n_processes = 2
     processes = list()
 
     for i in range(n_processes):
